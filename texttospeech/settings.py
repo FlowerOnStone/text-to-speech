6653b88552481863123918d33e992f63d87d5f2c
FlowerOnStone <thuanbn03@gmail.com>
2022-10-24 19:52:01 +0700

    change debug

--- texttospeech/settings.py
+++ texttospeech/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t#tgtacav67f30i=s$90m#=wqj@-@m)p(_&qd-2+pu)tyu438m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
